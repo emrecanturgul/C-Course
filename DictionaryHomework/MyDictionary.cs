c36187d50b580813b0f8ece39da1993a59d6822e
turgu <turgu@EMRE>
2023-12-20 23:12:35 +0300

    Add project files.

---
+++ DictionaryHomework/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryHomework
+{
+    internal class MyDictionary<X,Y>
+    {
+        X[] keys, temp1;
+        Y[] values, temp2;
+        public MyDictionary()
+        {
+
+            keys = new X[0];
+            values = new Y[0];
+
+        }
+        public void Add(X key, Y value)
+        {
+            X[] temp1 = keys; Y[] temp2 = values;
+            keys = new X[keys.Length + 1];
+            values = new Y[values.Length + 1];
+            for (int i = 0; i < temp1.Length; i++)
+            {
+                keys[i] = temp1[i];
+                values[i] = temp2[i];
+
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        public int _Length { get { return keys.Length; } }
+        public X[] _Keys { get { return keys; } }
+        public Y[] _Values { get { return values; } }
+    }
+}
---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("emre", 95);
+            myDictionary.Add("can", 87);
+
+            for (int i = 0; i < myDictionary._Length; i++)
+            {
+                Console.WriteLine($"\nkey = {myDictionary._Keys[i]} and  value = {myDictionary._Values[i]} ");
+            }
+        }
+    }
+}
