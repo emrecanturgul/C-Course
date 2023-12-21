1e74450ce188cb54124cc6992bdc967415dad3ec
turgu <turgu@EMRE>
2023-12-21 22:01:23 +0300

    c# course

---
+++ OOP2/IndividualCustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+     class IndividualCustomer:Customer
+    {
+       
+        public string IdNo { get; set; }
+       
+        public string Name { get; set; }
+        public string Surname { get; set; } 
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            
+        IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.CustomerNo = "123";
+            customer1.Id = 1; 
+            customer1.Name = "Emre";
+            customer1.Surname = "Turgul";
+            customer1.IdNo = "222222222"; 
+        CoorporateCustomer customer2 = new CoorporateCustomer();
+            customer2.CustomerNo = "124";
+            customer2.Id = 2;
+            customer2.CompanyName = "Turgul Comp.";
+            customer2.TaxNo = "123456789";
+        Customer customer3 = new IndividualCustomer();
+        Customer customer4 = new CoorporateCustomer(); 
+        CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer3);
+            customerManager.Add(customer4);
+                 
+
+        }
+    }
+}
+
